2de04628c4fe1f298a4778e3fd80aa3ff4be3505
Victor <victor@avria.co>
2019-12-20 13:11:19 -0600

    added react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return <div>Hello World</div>;
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App/>, mountNode);
